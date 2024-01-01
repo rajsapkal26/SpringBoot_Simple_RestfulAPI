31b5561fe671addb58dcdb1f93ab8d556703d43c
rajsapkal26 <rajsapkal26@gmail.com>
2024-01-01 14:34:09 +0530

    firts commmit

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	
+	private Integer id;
+	private String name;
+	private Double price;
+	
+		
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	public Product(Integer id, String name, Double price) {
+		
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		
+	}
+
+}
